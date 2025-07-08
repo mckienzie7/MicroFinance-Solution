fca31a85e586fda4cb80c632be7bb08e735aaca7
Michael Solomon <michaelelsa12@gmail.com>
2025-07-08 20:17:31 +0300

    new update

--- microfinance-frontend/src/services/apiClient.js
+++ microfinance-frontend/src/services/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:5000/api/v1', // Adjust this to your backend URL
+  baseURL: 'https://addismicrofinance.tech/api/v1', // Adjust this to your backend URL
   headers: {
     'Content-Type': 'application/json',
   },
