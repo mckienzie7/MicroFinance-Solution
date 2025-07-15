78e3e722805ec3ac82fec2118d12837799aae697
Michael Solomon <michaelelsa12@gmail.com>
2025-07-15 03:19:28 +0300

    UPDATE: Git push

--- BackEnd/Migrates/MFS.sql
+++ BackEnd/Migrates/MFS.sql
@@ -5,5 +5,5 @@ CREATE DATABASE IF NOT EXISTS MicroFinance_db;
 CREATE USER IF NOT EXISTS 'MFAdmin'@'localhost';
 ALTER USER 'MFAdmin'@'localhost' IDENTIFIED BY '12qwaszx@Q';
 GRANT ALL ON MicroFinance_db.* TO 'MFAdmin'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'MFAdmin'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'MFAdmin'@'%';
 FLUSH PRIVILEGES;
